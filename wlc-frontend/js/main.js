6ac1d5cf3a4171738e55ff9ea089426e8abb782e
Benny Neugebauer <bn@bennyn.de>
2013-12-01 18:29:12 +0100

    App-Logos & minified CSS

--- wlc-frontend/js/main.js
+++ wlc-frontend/js/main.js
@@ -1,10 +1,17 @@
 jQuery(document).ready(function($) {
 
-  var bodyClass = (navigator.userAgent.match(/(Android|iPad|iPhone|iPod|Windows Phone)/g) ? 'mobile' : 'desktop');
-  $('body').addClass(bodyClass);
+  function initHtmlBody() {
+    var bodyClass = (navigator.userAgent.match(/(Android|iPad|iPhone|iPod|Windows Phone)/g) ? 'mobile' : 'desktop');
+    $('body').addClass(bodyClass);
+  }
 
-  $('.sidebar-widget').on('touchstart', function() {
-    $(this).toggleClass('hover');
-  });
+  function initSidebarWidgets() {
+    $('.sidebar-widget').on('touchstart', function() {
+      $(this).toggleClass('hover');
+    });
+  }
+
+  initHtmlBody();
+  initSidebarWidgets();
 
 });
