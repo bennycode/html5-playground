a66961f2d37acf3c391ef3b6d21e58e7d81f07a3
Benny Neugebauer <bn@bennyn.de>
2013-12-01 03:42:42 +0100

    LESS comes into play ;)

--- wlc-frontend/js/main.js
+++ wlc-frontend/js/main.js
@@ -3,4 +3,8 @@ jQuery(document).ready(function($) {
   var bodyClass = (navigator.userAgent.match(/(Android|iPad|iPhone|iPod|Windows Phone)/g) ? 'mobile' : 'desktop');
   $('body').addClass(bodyClass);
 
+  $('.sidebar-widget').on('touchstart', function() {
+    $(this).toggleClass('hover');
+  });
+
 });
